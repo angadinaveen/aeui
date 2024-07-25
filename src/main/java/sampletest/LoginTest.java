3bf60ec3a6ff57e25b5c1f8546c9eab38678c92a
AUTOEDGECORP\Naveen.Angadi <naveen.angadi@automationedge.com>
2024-07-25 17:53:32 +0530

    adding comments

--- src/main/java/sampletest/LoginTest.java
+++ src/main/java/sampletest/LoginTest.java
@@ -1,5 +1,7 @@
 package sampletest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,10 +12,19 @@ public class LoginTest {
 	@Test
 	public void login() throws InterruptedException {
 		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		System.out.println("browsered opening");
 		driver.get("http://localhost:8080/aeui");
+		System.out.println("browsered opened");
+		System.out.println("username typing");
 		driver.findElement(By.xpath("//*[@id='uname']")).sendKeys("Tenant_2");
+		System.out.println("username typed");
+		System.out.println("password typing");
 		driver.findElement(By.xpath("//*[@id='pswd']")).sendKeys("Admin@123");
+		System.out.println("password typed");
+		System.out.println("clicking on login");
 		driver.findElement(By.xpath("//*[@id='signin1']")).click();
+		System.out.println("clicked on login");
 		Thread.sleep(5000);
 		
 		driver.quit();
